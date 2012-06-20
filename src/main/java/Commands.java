3a98794f957ac594d32da4ee839023d97721ec87
ahmet-celik <user@server>
2012-06-20 23:21:20 +0300

    First test

---
+++ src/main/java/Commands.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 20.06.2012
+ * Time: 15:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Commands {
+}
