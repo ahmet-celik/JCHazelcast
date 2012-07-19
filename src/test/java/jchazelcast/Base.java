662b745f8f3e4b7335f0f33a75391186a092e7f2
ahmet-celik <celikeins@hotmail.com>
2012-07-19 11:29:40 +0300

    new test added

--- src/test/java/jchazelcast/Base.java
+++ src/test/java/jchazelcast/Base.java
@@ -3,12 +3,12 @@ package jchazelcast;
 import com.hazelcast.core.Hazelcast;
 import org.junit.*;
 
-import java.io.IOException;
+
 import java.io.UnsupportedEncodingException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class testAuth {
+public class Base {
     public final static int NUM =  2;
     public static ArrayList<JCHazelcast> clientPool=new ArrayList<JCHazelcast>();
 
@@ -30,69 +30,6 @@ public class testAuth {
             clientPool.add(new JCHazelcast());
     }
 
-
-
-    public void mapPutAll(String name){
-        Map map = new HashMap();
-        map.put("key","data");
-        map.put("key2","data2");
-        map.put("key3","data3");
-        JCMap m = clientPool.get(0).getMap(name);
-        System.out.println(name+"*** "+m.putAll("putall", false, map));
-    }
-
-
-    @Test
-    public void listenerTest() throws InterruptedException {
-       mapPutAll("a");
-        JCMap<String,String> m =  clientPool.get(0).getMap("a");
-        (new Thread(new Runnable() {
-            @Override
-            public void run() {
-                 JCMap<String,String> mp =  clientPool.get(1).getMap("a");
-                  mp.addMapListener(new MyListener("ahmet"),true);
-            }
-        })).start();
-        Thread.sleep(2000);
-        m.put("0",false,"key","data0");
-        m.put("0",false,"key4","dataX");
-        m.remove("0",false,"key");
-        m.evict("0",false,"key2");
-
-
-    }
-
-    public static class MyListener extends JCMapListener{
-        String n;
-        MyListener(String name){
-              n=name;
-        }
-
-        @Override
-        public void entryUpdated(EntryEvent e)  {
-            System.out.println(n+" listens(updated) "+e.getListenedStructureName()+" key: "+e.getKey()+" value: "+e.getValue());
-
-        }
-
-        @Override
-        public void entryAdded(EntryEvent e) {
-            System.out.println(n+" listens(added) "+e.getListenedStructureName()+" key: "+e.getKey()+" value: "+e.getValue());
-            this.removeMapListener(e);
-
-        }
-
-        @Override
-        public void entryRemoved(EntryEvent e)  {
-            System.out.println(n+" listens(removed) "+e.getListenedStructureName()+" key: "+e.getKey()+" value: "+e.getValue());
-
-        }
-
-        @Override
-        public void entryEvicted(EntryEvent e) {
-            System.out.println(n+" listens(evict) "+e.getListenedStructureName()+" key: "+e.getKey()+" value: "+e.getValue());
-
-        }
-    }
 
     @Test
     public void perf() throws InterruptedException {
@@ -144,20 +81,7 @@ public class testAuth {
     }
 
 
-    //seems OK.
-    @Test
-    public  void atomicTest(){
-       JCAtomicNumber a1 = clientPool.get(0).getAtomicNumber("1");
-        JCAtomicNumber a2 = clientPool.get(0).getAtomicNumber("2");
 
-        System.out.println(a1.addAndGet("0", 15));
-        System.out.println(a1.getAndAdd("1",5));
-        System.out.println(a1.compareAndSet("2",20,3));
-        System.out.println(a2.getAndAdd("7", 5));
-        System.out.println(a2.compareAndSet("3",5,8));
-        System.out.println(a2.getAndSet("4", 3));
-        System.out.println(a2.addAndGet("5", 10));
-    }
 
 
     @Test
@@ -212,35 +136,6 @@ public class testAuth {
     public void sadd(){
         JCSet<Integer> s = clientPool.get(0).getSet("2");
         s.add("s",false,45);
-    }
-
-
-    //gives errors.
-    @Test
-    public void locks(){
-        JCLock lk = clientPool.get(0).getLock("2");
-        System.out.println(lk.lock("1"));
-        System.out.println(lk.isLocked("2"));
-        System.out.println(lk.unlock("3"));
-        System.out.println(lk.isLocked("4"));
-        System.out.println(lk.lock("5"));
-        System.out.println(lk.forceUnlock("1"));
-    }
-
-    @Test
-    public void queue() throws InterruptedException {
-        JCQueue q = clientPool.get(0).getQueue("qu");
-        System.out.println(q.offer("1",false,0,34));  //ok
-        System.out.println(q.offer("1",false,0,38));  //ok
-
-        // System.out.println(q.peek("2"));              //ok
-       // System.out.println(q.poll("3", 0));
-        System.out.println(q.offer("1",false,0,42));  //ok
-
-        System.out.println(q.size("4"));
-        System.out.println(q.remove("4.5", false, 38));
-        System.out.println(q.remainingCapacity("5"));
-        System.out.println(q.entries("6"));
     }
 
     public static class MyItm extends ItemListener{
