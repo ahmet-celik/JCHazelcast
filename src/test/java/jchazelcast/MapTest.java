2dedc6033d45c4a3d5a246dbeff6a786ae83e6c5
ahmet-celik <celikeins@hotmail.com>
2012-07-19 10:02:34 +0300

    readme added

--- src/test/java/jchazelcast/MapTest.java
+++ src/test/java/jchazelcast/MapTest.java
@@ -4,6 +4,8 @@ package jchazelcast;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public class MapTest extends testAuth {
@@ -64,7 +66,7 @@ public class MapTest extends testAuth {
         })).start();
     }
 
-    void lockFAT(final String n,final String key) {
+    void lockUnlockFAT(final String n,final String key) {
         (new Thread(new Runnable() {
             @Override
             public void run()  {
@@ -83,12 +85,12 @@ public class MapTest extends testAuth {
     }
 
     @Test
-    public void tryPut() throws InterruptedException {
+    public void tryPutRemove() throws InterruptedException {
         JCMap m = clientPool.get(0).getMap("ahmet");
                 mapPut();
         cdl = new CountDownLatch(1);
         cdl2 = new CountDownLatch(1);
-        lockFAT("ahmet","key");
+        lockUnlockFAT("ahmet", "key");
         cdl.await();
         Assert.assertTrue(m.isKeyLocked(f,"key"));
         Assert.assertFalse(m.tryPut(f, 2000, "key", "dat2"));
@@ -97,6 +99,7 @@ public class MapTest extends testAuth {
         cdl.await();
         Assert.assertFalse(m.isKeyLocked(f, "key"));
         Assert.assertTrue(m.tryPut(f,2000,"key","dat2"));
+        Assert.assertTrue(m.tryRemove(f,0,"key").equals("dat2"));
     }
 
     @Test
@@ -108,6 +111,71 @@ public class MapTest extends testAuth {
         Assert.assertEquals(m.get(f,"key"),"datx");
     }
 
+    @Test
+    public void tryLockNGet(){
+        JCMap m =  clientPool.get(0).getMap("ahmet");
+        mapPut();
+        Assert.assertTrue(m.tryLockAndGet(f,0,"key").equals("dat1"));
+    }
+
+    @Test
+    public void getAllKeySet(){
+        JCMap m =  clientPool.get(0).getMap("ahmet");
+        mapPut();
+        m.put(f, a, "key2", "data2");
+        Assert.assertEquals(m.remove(f,a,"key2"),"data2");
+        Assert.assertTrue(m.getAll(f, m.keySet(f, "map")).contains("dat1"));
+    }
+
+    @Test
+    public void entry(){
+        JCMap m =  clientPool.get(0).getMap("ahmet");
+        mapPut();
+        JCMapEntry me = m.getEntry(f, "key");
+        Assert.assertTrue(me.getKey().equals("key") && me.getValue().equals("dat1"));
+    }
+
+    void lockFAT(final String n,final String key) {
+        (new Thread(new Runnable() {
+            @Override
+            public void run()  {
+                JCMap m = clientPool.get(1).getMap(n);
+                Assert.assertTrue(m.tryLock(f, 0, key));
+                cdl.countDown();
+            }
+        })).start();
+    }
+
+    @Test
+    public void forceUnlock() throws InterruptedException {
+        JCMap m = clientPool.get(0).getMap("ahmet");
+        mapPut();
+        cdl = new CountDownLatch(1);
+        lockFAT("ahmet", "key");
+        cdl.await();
+        Assert.assertTrue(m.forceUnlock(f, "key"));
+        Assert.assertTrue(m.tryPut(f, 2000, "key", "dat2"));
+    }
+
+    @Test
+    public void contains(){
+        JCMap m = clientPool.get(0).getMap("ahmet");
+        mapPut();
+        Assert.assertNull(m.putIfAbsent(f, a, "keyX", "datX"));
+        Assert.assertEquals(m.putIfAbsent(f,a,"key","dat2"),"dat1");
+        Assert.assertEquals(m.get(f,"key"),"dat1");
+        Assert.assertTrue(m.containsKey(f,"key"));
+        Assert.assertTrue(m.containsValue(f,"dat1"));
+    }
+
+    @Test
+    public void flushNEvict(){
+        JCMap m = clientPool.get(0).getMap("ahmet");
+        mapPut();
+        Assert.assertTrue(m.evict(f,a,"key"));
+        Assert.assertTrue(m.flush(f,a));
+
+    }
 
 
 
